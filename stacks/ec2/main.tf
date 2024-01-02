526a6053920367b2bc4349c7bb6841102294f14c
Tim Davis <tim.davis.instinct@gmail.com>
2024-01-01 21:16:32 -0600

    Update code for AWS instance

--- stacks/ec2/main.tf
+++ stacks/ec2/main.tf
@@ -1,5 +1,25 @@
-# Terraform >= 0.12
-resource "aws_instance" "main" {
-  # ...
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
