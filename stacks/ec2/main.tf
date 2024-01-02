bfba5f57c5b6ae0b07fcf90829ea130d06ef96f6
Tim Davis <tim.davis.instinct@gmail.com>
2024-01-02 08:45:25 -0600

    Updates

--- stacks/ec2/main.tf
+++ stacks/ec2/main.tf
@@ -12,11 +12,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -35,7 +30,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  subnet_id = "data.terraform_remote_state.vpc.outputs.subnet_id"
+  subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
   instance_type = "t3.micro"
 
   tags = {
