71fe479ec7583158bfa45ed790251c5e4b99c26b
Tim Davis <tim.davis.instinct@gmail.com>
2024-01-02 08:46:53 -0600

    Remove extra VPC's

--- stacks/vpc/main.tf
+++ stacks/vpc/main.tf
@@ -12,11 +12,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
